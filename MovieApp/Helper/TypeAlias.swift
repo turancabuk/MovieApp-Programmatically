7985c733f4e70c6331e3d41b5e8797085c4eb246
turan <turan6677@gmail.com>
2022-10-26 23:21:43 +0300

    helper- typealiase configureTableView created. homeViewCell created.

---
+++ MovieApp/Helper/TypeAlias.swift
@@ -0,0 +1,10 @@
+//
+//  TypeAlias.swift
+//  MovieApp
+//
+//  Created by Turan Çabuk on 26.10.2022.
+//
+
+import UIKit.UIViewController
+
+typealias configureTableView = UITableViewDelegate & UITableViewDataSource
